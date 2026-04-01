089f5041ffefdaa87d06d2d8bd2b0e29b2b1dfe6
Abdallah-Sarg <modybody120@gmail.com>
2026-04-01 21:52:44 +0200

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,31 @@
+﻿const db = require('../lib/db');
+const { createDashboardApp } = require('../web/dashboard');
+
+let initPromise = null;
+let app = null;
+
+async function ensureReady() {
+  if (!initPromise) {
+    initPromise = db.init().catch((err) => {
+      initPromise = null;
+      throw err;
+    });
+  }
+  await initPromise;
+  if (!app) {
+    app = createDashboardApp(null);
+  }
+  return app;
+}
+
+module.exports = async (req, res) => {
+  try {
+    const readyApp = await ensureReady();
+    return readyApp(req, res);
+  } catch (err) {
+    console.error('vercel dashboard bootstrap error:', err);
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.end(JSON.stringify({ error: 'dashboard_bootstrap_failed' }));
+  }
+};
